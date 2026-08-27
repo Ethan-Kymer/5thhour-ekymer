cbe2324da0df87e99c7873fca25eea2bfa1c22a6
ethan kymer <ek127415@student.wpsok.org>
2026-08-27 13:01:46 -0500

    completed homework 2 and 3

---
+++ HW2.py
@@ -0,0 +1,35 @@
+#Ethan kymer
+#class- 5th hour
+# homework 2
+
+print("Hello World")
+
+intvar1=4
+stringvar2=("box")
+booleanVar3 = True
+stringVar4 = "Nineteen"
+stringVar5 = "Rope"
+intVar6 = 19
+stringVar7 = "19"
+intVar8 = 2
+booleanVar9 = True
+booleanVar10 = False
+stringVar11 = "True"
+stringVar12 = "False"
+intVar13 = 5
+stringVar14 = "The"
+stringVar15 = "Game"
+booleanVar16 = False
+intVar17 = 3
+intVar18 = 2
+intVar19 = 1
+stringVar20 = stringVar11
+stringVar21 = stringVar12
+stringVar22 = stringVar4
+stringVar23 = stringVar14
+stringVar24 = stringVar15
+intVar25 = 6
+
+#3. Take all the variables you labeled as "integer", add them together, and print the result.
+
+print(intvar1 + intVar6 + intVar8 + intVar13 + intVar17 + intVar18 + intVar19 + intVar25)
